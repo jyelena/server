84aede23fe88bdfe739469fd7817accd48279b85
dmikhaylov <d.y.mikhayov@yandex.ru>
2021-03-07 13:06:54 +0300

    исправил поведение скрипта, изменяющего autoindex

--- srcs/a_change.sh
+++ srcs/a_change.sh
@@ -1,2 +1,4 @@
-sed -i {'s/autoindex off/autoindex on/'} /etc/nginx/sites-available/ftsite.conf
+cat /etc/nginx/sites-available/ftsite.conf | grep autoindex
+sed -i {'s/autoindex on/autoindex off/'} /etc/nginx/sites-available/ftsite.conf
+cat /etc/nginx/sites-available/ftsite.conf | grep autoindex
 service nginx reload
